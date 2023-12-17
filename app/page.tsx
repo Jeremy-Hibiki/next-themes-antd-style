cc03268532199fb9598415a0b6c601b5ba51ead0
Jeremy-Hibiki <JeremyJiang430@outlook.com>
2023-12-18 02:24:50 +0800

    feat: light/dark theme switch & primary color picker

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,82 @@
 'use client';
 
-import { Skeleton, theme as antdTheme } from 'antd';
+import { ColorPicker, ConfigProvider, Flex, Form, Segmented, Skeleton, Switch, theme as antdTheme } from 'antd';
 import ComponentDemoPro from 'antd-token-previewer/es/token-panel-pro/ComponentDemoPro';
-import { Suspense } from 'react';
+import { useTheme } from 'next-themes';
+import { Suspense, useEffect, useState } from 'react';
 
 export default function Home() {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [primary, setPrimary] = useState('#e93fc4');
+
+  const { token } = antdTheme.useToken();
+
+  const [isMount, setIsMount] = useState(false);
+  useEffect(() => {
+    setIsMount(true);
+  }, []);
+
   return (
-    <Suspense fallback={<Skeleton style={{ margin: 24 }} />}>
-      <ComponentDemoPro
-        theme={{
-          name: 't',
-          key: 't',
-          config: antdTheme.defaultConfig,
+    isMount && (
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <ConfigProvider theme={{
+          token: {
+            colorPrimary: primary,
+          },
         }}
-        style={{
-          width: '100vw',
-          height: '100vh',
-        }}
-      />
-    </Suspense>
+        >
+          <Flex
+            justify="center"
+            align="center"
+            gap={16}
+            style={{
+              height: 48,
+              background: '#ccc6',
+            }}
+          >
+            <Form layout="inline">
+              <Form.Item label="主色">
+                <ColorPicker showText value={primary} onChangeComplete={color => setPrimary(color.toHexString())} />
+              </Form.Item>
+
+              <Form.Item label="明暗">
+                <Flex justify="center" align="center" gap={16}>
+                  <Segmented
+                    options={['light', 'dark']}
+                    value={resolvedTheme}
+                    onChange={(v) => {
+                      setTheme(v as string);
+                    }}
+                  />
+                  <Switch
+                    checked={resolvedTheme === 'dark'}
+                    onChange={(v) => {
+                      setTheme(v ? 'dark' : 'light');
+                    }}
+                  />
+                </Flex>
+              </Form.Item>
+            </Form>
+          </Flex>
+        </ConfigProvider>
+        <ComponentDemoPro
+          theme={{
+            name: 't',
+            key: 't',
+            config: {
+              token: {
+                ...token,
+                colorPrimary: primary,
+              },
+              algorithm: resolvedTheme === 'dark' ? antdTheme.darkAlgorithm : antdTheme.defaultAlgorithm,
+            },
+          }}
+          style={{
+            width: '100%',
+            height: 'calc(100vh - 48px)',
+          }}
+        />
+      </div>
+    )
   );
 }
